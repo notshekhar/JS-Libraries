e2de1a8f78a821d5b9080465cec7baefd59cd204
Shekhar Tyagi <notshekhar@gmail.com>
2021-02-05 09:19:11 +0530

    new

--- array.js
+++ array.js
@@ -1,15 +1,19 @@
-//function to reshape the array 
+//function to reshape the array
 Array.prototype.reshape = function (rows, cols) {
-    var copy = this.slice(0); // Copy all elements.
-    this.length = 0; // Clear out existing array.
+    var copy = this.slice(0) // Copy all elements.
+    this.length = 0 // Clear out existing array.
     for (var r = 0; r < rows; r++) {
-        var row = [];
+        var row = []
         for (var c = 0; c < cols; c++) {
-            var i = r * cols + c;
+            var i = r * cols + c
             if (i < copy.length) {
-                row.push(copy[i]);
+                row.push(copy[i])
             }
         }
-        this.push(row);
+        this.push(row)
     }
+}
+
+Array.prototype.copy = function () {
+    return this.map((e) => e)
 }
