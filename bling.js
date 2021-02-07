4a701c06991a025c913ee0c123aa93fe94ac8428
Shekhar Tyagi <notshekhar@gmail.com>
2021-02-07 23:11:31 +0530

    new

--- bling.js
+++ bling.js
@@ -1,9 +1,26 @@
-window.$ = document.querySelectorAll.bind(document)
+window.$ = document.querySelector.bind(document)
+Element.prototype.on = Element.prototype.addEventListener
+function createElement(e, attrs) {
+    let el = document.createElement(e)
+    for (let attr in attrs) {
+        const value = attrs[attr]
+        if (attr == "innerText") el.innerText = value
+        else if (attr == "innerHTML") el.innerHTML = value
+        else el.setAttribute(attr, value)
+    }
+    return el
+}
+Element.prototype.css = function (styles) {
+    for (let style in styles) {
+        this.style[style] = styles[style]
+    }
+}
 
+//
+window._ = document.querySelectorAll.bind(document)
 Node.prototype.on = window.on = function (name, fn) {
     this.addEventListener(name, fn)
 }
-
 NodeList.prototype.__proto__ = Array.prototype
 
 NodeList.prototype.on = NodeList.prototype.addEventListener = function (
@@ -20,4 +37,7 @@ NodeList.prototype.css = function (styles) {
             el.style[style] = styles[style]
         }
     })
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
+    this.forEach((el) => el.remove())
 }
