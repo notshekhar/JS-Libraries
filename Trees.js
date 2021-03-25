f0f20b1f11cd50b7a62cdf0cc48885614e2f89c0
Shekhar Tyagi <notshekhar@gmail.com>
2021-03-25 22:02:53 +0530

    new

---
+++ Trees.js
@@ -0,0 +1,35 @@
+function Node() {
+    this.values = null
+    this.end = false
+    this.add = (v, t) => {
+        if (!this.values) this.values = {}
+        if (!this.values[v]) this.values[v] = new Node()
+    }
+}
+function Trees() {
+    this.root = new Node()
+    this.add = (s) => {
+        let temp = this.root
+        for (let i = 0; i < s.length; i++) {
+            temp.add(s[i])
+            if (i == s.length - 1) temp.values[s[i]].end = true
+            temp = temp.values[s[i]]
+        }
+    }
+    this.search = (s) => {
+        let temp = this.root
+        for (let i = 0; i < s.length; i++) {
+            if (!temp.values) return false
+            if ((i == s.length - 1) & !temp.values[s[i]].end) return false
+            if (!temp.values[s[i]]) return false
+            temp = temp.values[s[i]]
+        }
+        return true
+    }
+}
+
+let trees = new Trees()
+trees.add("shekhar")
+trees.add("she")
+console.log(trees.search("shek"))
+console.log(trees)
