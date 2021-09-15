ccb7520ff2b55e9c7a2691e8f25fffe950d83a9b
Shekhar Tyagi <notshekhar@gmail.com>
2021-09-15 19:39:56 +0530

    new

--- css.js
+++ css.js
@@ -1,5 +1,5 @@
 //get and set css variables
-function cssv(name, value) {
+function css(name, value) {
     if (name[0] != "-") name = "--" + name //allow passing with or without --
     if (value) document.documentElement.style.setProperty(name, value)
     return getComputedStyle(document.documentElement).getPropertyValue(name)
